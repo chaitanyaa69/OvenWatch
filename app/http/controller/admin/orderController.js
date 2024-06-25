d91b4b255047dec46c045de6ebbeb62da9f4b7b1
Chaitanya Reddy <reddy03042004@gmail.com>
2024-06-25 10:48:47 +0530

    added admin order table, place order functionality

--- app/http/controller/admin/orderController.js
+++ app/http/controller/admin/orderController.js
@@ -1,21 +1,22 @@
-const Order = require('../../../models/order')
+const Order = require('../../../models/order');
 
 function orderController() {
   return {
-    index(req, res) {
+    async index(req, res) {
       try {
-        Order.find({ status: { $ne: 'completed' } },null, { sort: { 'createdAt': -1 }}).populate('customerId', '-password');
+        const orders = await Order.find({ status: { $ne: 'completed' } }, null, { sort: { 'createdAt': -1 } }).populate('customerId', '-password');
+        
         if (req.xhr) {
-          return res.json(Orders);
+          return res.json(orders);
         } else {
-          return res.render('admin/orders');
+          return res.render('admin/orders', { orders });  // Pass orders to the view
         }
       } catch (err) {
         console.error(err);
-        // Handle the error appropriately
+        res.status(500).send('Internal Server Error');  // Send an appropriate error response
       }
     }
   }
 }
 
-module.exports = orderController
+module.exports = orderController;
